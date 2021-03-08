62bf09ca5eb571db16fdcfd08e6d63e3fb4f83ff
Stephen1902 <s_nichols_other@yahoo.co.uk>
2021-03-08 10:26:34 +0000

    Initial Commit

---
+++ Source/HouseDefender/HouseDefenderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HouseDefenderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOUSEDEFENDER_API AHouseDefenderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
